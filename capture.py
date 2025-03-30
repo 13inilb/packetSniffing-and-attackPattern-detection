a332dd3843e6a607f92ea70d0a1ee1f66f8948b5
Binil Babu P <13inilb@gmail.com>
2025-03-30 07:46:22 +0530

    Add files via upload

--- capture.py
+++ capture.py
@@ -1,52 +1,42 @@
-import threading
-from scapy.all import sniff, wrpcap,  Ether, IP
-import time
-import socket
+from scapy.all import sniff, PcapWriter
+import sqlite3
 
-packets =[]
 
-def packet_handler(packet):
-        #inform about the captured packet
-        if packet.haslayer('IP'):
-            print(f"[+] packet captured from {packet['IP'].src} --> to {packet['IP'].dst}")
+# Initialize database
+conn = sqlite3.connect("packets.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS packets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        src_ip TEXT,
+        dst_ip TEXT,
+        protocol TEXT,
+        status TEXT,
+        analysis TEXT
+    )
+""")
+conn.commit()
 
-        packets.append(packet)  # Append the packet to the list
 
-        
-def save_pcap():
-    global packets
-    while True:
-        if packets:
-            wrpcap("captured_packets.pcap", packets, append=True)  # Append packets
-            print(f"Saved {len(packets)} packets to 'captured_packets.pcap'")
-            packets = []  # Clear packet list after saving
-        time.sleep(2)  # Delay for 2 seconds
+def process_packet(packet, pcap_writer):
+    if packet.haslayer('IP'):
+        src_ip = packet['IP'].src
+        dst_ip = packet['IP'].dst
+        protocol = packet.sprintf('%IP.proto%')
 
-def get_system_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(("1.1.1.1", 80))
-        return s.getsockname()[0]
+        cursor.execute("INSERT INTO packets (timestamp, src_ip, dst_ip, protocol ) VALUES (datetime('now'), ?, ?, ?)",
+                       (src_ip, dst_ip, protocol))
+        conn.commit()
+        print(f"Captured: {src_ip} -> {dst_ip} ({protocol})")
+        pcap_writer.write(packet)
 
+
+def capture(pcap_file):
+    with PcapWriter(pcap_file, append=True, sync=True) as pcap_writer:
+        sniff(prn=lambda pkt: process_packet(pkt, pcap_writer), store=False, count = 50)
 
 
 def main():
-    sys_ip = get_system_ip()
-    print(f"Sniffing started....")
-    print(f"Listening for incoming packets on IP: {sys_ip}")
-
-    dummy_packet = Ether() / IP(dst="0.0.0.0")
-    wrpcap("captured_packets.pcap", dummy_packet)  # Create file with a dummy packet
-
-    try:
-        threading.Thread(target=save_pcap, daemon=True).start()
-        sniff(prn=packet_handler, filter=f"dst host {sys_ip}", store=False )
-        
-    except KeyboardInterrupt:
-            print(f"\nPacket capture stopped")
-        
-
-    
-    
-
-if __name__ == "__main__":   
-    main() 
+    pcap_file = "captured.pcap"
+    capture(pcap_file)
