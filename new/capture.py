8448c0f99759e7282148a0d1ebc180c02a86a18f
Binil Babu P <13inilb@gmail.com>
2025-03-31 07:38:21 +0530

    Create capture.py

---
+++ new/capture.py
@@ -0,0 +1,50 @@
+import datetime
+from scapy.all import sniff, PcapWriter
+import sqlite3
+
+
+# Initialize database
+conn = sqlite3.connect("packets.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS packets (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp TEXT,
+        src_ip TEXT,
+        dst_ip TEXT,
+        protocol TEXT,
+        status TEXT,
+        analysis TEXT
+    )
+""")
+conn.commit()
+
+
+def process_packet(packet, pcap_writer):
+    if packet.haslayer('IP'):
+        timestamp = datetime.datetime.fromtimestamp(packet.time)
+        src_ip = packet['IP'].src
+        dst_ip = packet['IP'].dst
+        protocol = packet.sprintf('%IP.proto%')
+
+        cursor.execute("INSERT INTO packets (timestamp, src_ip, dst_ip, protocol ) VALUES (?, ?, ?, ?)",
+                       (timestamp,src_ip, dst_ip, protocol))
+
+        conn.commit()
+        print(f"Captured: {src_ip} -> {dst_ip} ({protocol})")
+        pcap_writer.write(packet)
+
+
+def capture(pcap_file):
+    with PcapWriter(pcap_file, append=True, sync=True) as pcap_writer:
+        sniff(iface = "enp0s8",prn=lambda pkt: process_packet(pkt, pcap_writer), store=False )
+
+
+def main():
+
+    pcap_file = "captured.pcap"
+    print("Starting Capture...")
+    capture(pcap_file)
+
+if __name__ == "__main__":   
+    main() 
